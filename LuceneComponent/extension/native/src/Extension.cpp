230441e2259c35e4f73911411c61ca2ac05fec1a
nimrod97@gmail.com <nimrod97@gmail.com@9d3c0a1a-df6d-9ad0-c7db-03087358127c>
2012-08-07 16:48:56 +0000

    add

--- LuceneComponent/extension/native/src/Extension.cpp
+++ LuceneComponent/extension/native/src/Extension.cpp
@@ -18,85 +18,74 @@ using namespace lucene::search;
 using namespace lucene::store;
 
 
-#ifdef LOG_ENABLED
-    #define DEBUG_PRINT(msg) print(msg)
-    #define DEBUG_RESULT(method, result) printResult(method, result)
-#else
-    #define DEBUG_PRINT(msg)
-    #define DEBUG_RESULT(method, result)
-#endif
-
-
-void print(const char * msg)
+void print(const char * logfile, const char * filename, long line, const char * funcname, const char * msg, ...)
 {
-    if(LOG_ENABLED)
-    {
-        FILE *file;
-        char fname[] = LOG_FILE;
-        file = fopen(fname,"a");
-
-        if(file == NULL)
-        {
-            return;
-        }
-
-        fprintf(file, "%s", msg);
-        fprintf(file, "\n");
-
-        fclose(file);
-    }
+   FILE *file;
+   struct tm *current;
+   time_t now;
+   file = fopen(logfile,"a");
+   if(file == NULL)
+   {
+       return;
+   }
+   time(&now);
+   current = localtime(&now);
+   fprintf(file, "%2i:%2i:%2i [%s:%ld] %s - ", current->tm_hour, current->tm_min, current->tm_sec, filename, line, funcname);
+   va_list args;
+   va_start (args, msg);
+   vfprintf (file, msg, args);
+   va_end (args);
+   fprintf(file, "\n");
+   fclose(file);
 }
 
-void printResult(const char * method, FREResult result)
-{
-    char msg [1024];
 
+void printResult(const char * logfile, const char * filename, long line, const char * funcname, const char * method, FREResult result)
+{
     if(FRE_OK == result)
     {
-        sprintf(msg, "SUCCESS FRE_OK: %s", method);
+        print(logfile, filename, line, funcname, "SUCCESS FRE_OK: %s", method);
     }
     else if(FRE_NO_SUCH_NAME == result)
     {
-        sprintf(msg, "ERROR FRE_NO_SUCH_NAME: %s", method);
+        print(logfile, filename, line, funcname, "ERROR FRE_NO_SUCH_NAME: %s", method);
     }
     else if(FRE_INVALID_OBJECT == result)
     {
-        sprintf(msg, "ERROR FRE_INVALID_OBJECT: %s", method);
+        print(logfile, filename, line, funcname, "ERROR FRE_INVALID_OBJECT: %s", method);
     }
     else if(FRE_TYPE_MISMATCH == result)
     {
-        sprintf(msg, "ERROR FRE_TYPE_MISMATCH: %s", method);
+        print(logfile, filename, line, funcname, "ERROR FRE_TYPE_MISMATCH: %s", method);
     }
     else if(FRE_ACTIONSCRIPT_ERROR == result)
     {
-        sprintf(msg, "ERROR FRE_ACTIONSCRIPT_ERROR: %s", method);
+        print(logfile, filename, line, funcname, "ERROR FRE_ACTIONSCRIPT_ERROR: %s", method);
     }
     else if(FRE_INVALID_ARGUMENT == result)
     {
-        sprintf(msg, "ERROR FRE_INVALID_ARGUMENT: %s", method);
+        print(logfile, filename, line, funcname, "ERROR FRE_INVALID_ARGUMENT: %s", method);
     }
     else if(FRE_INVALID_ARGUMENT == result)
     {
-        sprintf(msg, "ERROR FRE_INVALID_ARGUMENT: %s", method);
+        print(logfile, filename, line, funcname,  "ERROR FRE_INVALID_ARGUMENT: %s", method);
     }
     else if(FRE_READ_ONLY == result)
     {
-        sprintf(msg, "ERROR FRE_READ_ONLY: %s", method);
+        print(logfile, filename, line, funcname, "ERROR FRE_READ_ONLY: %s", method);
     }
     else if(FRE_WRONG_THREAD == result)
     {
-        sprintf(msg, "ERROR FRE_WRONG_THREAD: %s", method);
+        print(logfile, filename, line, funcname, "ERROR FRE_WRONG_THREAD: %s", method);
     }
     else if(FRE_ILLEGAL_STATE == result)
     {
-        sprintf(msg, "ERROR FRE_ILLEGAL_STATE: %s", method);
+        print(logfile, filename, line, funcname, "ERROR FRE_ILLEGAL_STATE: %s", method);
     }
     else if(FRE_INSUFFICIENT_MEMORY == result)
     {
-        sprintf(msg, "ERROR FRE_INSUFFICIENT_MEMORY: %s", method);
+        print(logfile, filename, line, funcname, "ERROR FRE_INSUFFICIENT_MEMORY: %s", method);
     }
-
-    print(msg);
 }
 
 /*
@@ -120,7 +109,7 @@ FREObject get_null_string_as_empty(const char * resultString)
     {
        result = FRENewObjectFromUTF8((uint32_t)strlen(""), (const uint8_t*)"", &stringObject);
     }
-    DEBUG_RESULT("FRENewObjectFromUTF8: get_null_string_as_empty", result);
+    DEBUG_RESULT(__FILE__, __LINE__, __FUNCTION__, "FRENewObjectFromUTF8: get_null_string_as_empty", result);
     return stringObject;
 }
 
@@ -132,11 +121,11 @@ void put_object_property_int32(FREObject object, const uint8_t * propertyName, u
 
     result = FRENewObjectFromUint32(value, &propertyValue);
     sprintf(msg, "FRENewObjectFromUint32: %s", propertyName);
-    DEBUG_RESULT(msg, result);
+    DEBUG_RESULT(__FILE__, __LINE__, __FUNCTION__, msg, result);
 
     result = FRESetObjectProperty(object, propertyName, propertyValue, NULL);
     sprintf(msg, "FRESetObjectProperty: %s", propertyName);
-    DEBUG_RESULT(msg, result);
+    DEBUG_RESULT(__FILE__, __LINE__, __FUNCTION__, msg, result);
 }
 
 void put_object_property_strw(FREObject object, const uint8_t * propertyName, const wchar_t * value)
@@ -149,7 +138,7 @@ void put_object_property_strw(FREObject object, const uint8_t * propertyName, co
         FREObject propertyValue = get_null_string_as_empty(string);
         FREResult result = FRESetObjectProperty(object, propertyName, propertyValue, NULL);
         sprintf(msg, "FRESetObjectProperty: %s", propertyName);
-        DEBUG_RESULT(msg, result);
+        DEBUG_RESULT(__FILE__, __LINE__, __FUNCTION__, msg, result);
         free(string);
     }
 }
@@ -161,7 +150,7 @@ FREObject get_result_as_boolean(bool value)
 {
     FREObject resultObject;
     FREResult result = FRENewObjectFromBool(value, &resultObject);
-    DEBUG_RESULT("FRENewObjectFromBool: get_result_as_boolean", result);
+    DEBUG_RESULT(__FILE__, __LINE__, __FUNCTION__, "FRENewObjectFromBool: get_result_as_boolean", result);
     return resultObject;
 }
 
@@ -172,7 +161,7 @@ FREObject get_result_as_int32(int value)
 {
     FREObject resultObject;
     FREResult result = FRENewObjectFromInt32(value, &resultObject);
-    DEBUG_RESULT("FRENewObjectFromInt32: get_result_as_int32", result);
+    DEBUG_RESULT(__FILE__, __LINE__, __FUNCTION__, "FRENewObjectFromInt32: get_result_as_int32", result);
     return resultObject;
 }
 
@@ -181,16 +170,14 @@ FREObject get_result_as_int32(int value)
 */
 FREObject test(FREContext ctx, void* funcData, uint32_t argc, FREObject argv[])
 {
-    DEBUG_PRINT("test");
+    DEBUG_PRINT(__FILE__, __LINE__, __FUNCTION__, "test");
 
     return get_result_as_boolean(true);
 }
 
 void contextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, uint32_t* numFunctions, const FRENamedFunction** functions)
 {
-    char msg [1024];
-    sprintf(msg, "contextInitializer: %s", (char *)ctxType);
-    DEBUG_PRINT(msg);
+    DEBUG_PRINT(__FILE__, __LINE__, __FUNCTION__, "contextInitializer: %s", (char *)ctxType);
 
     *numFunctions = 1;
 
@@ -205,14 +192,13 @@ void contextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, u
 
 void contextFinalizer(FREContext ctx)
 {
-    DEBUG_PRINT("contextFinalizer");
-    //close open device if any
+    DEBUG_PRINT(__FILE__, __LINE__, __FUNCTION__, "contextFinalizer");
     return;
 }
 
 void initializer(void** extData, FREContextInitializer* ctxInitializer, FREContextFinalizer* ctxFinalizer)
 {
-    DEBUG_PRINT("initializer");
+    DEBUG_PRINT(__FILE__, __LINE__, __FUNCTION__, "initializer");
     *ctxInitializer = &contextInitializer;
     *ctxFinalizer = &contextFinalizer;
 }
@@ -221,6 +207,6 @@ void initializer(void** extData, FREContextInitializer* ctxInitializer, FREConte
 //unload the extension or call FREFinalizer().
 void finalizer(void* extData)
 {
-    DEBUG_PRINT("finalizer");
+    DEBUG_PRINT(__FILE__, __LINE__, __FUNCTION__, "finalizer");
     return;
 }
--- LuceneComponent/extension/native/src/Extension.h
+++ LuceneComponent/extension/native/src/Extension.h
@@ -1,9 +1,9 @@
 #ifndef UNICODE
-#define UNICODE
+    #define UNICODE
 #endif
 
 #ifndef _UNICODE
-#define _UNICODE
+    #define _UNICODE
 #endif
 
 #include <windows.h>
@@ -11,18 +11,29 @@
 #ifdef _WIN32
     //force using mingw <stdint.h>, see FlashRuntimeExtensions.h
     #undef WIN32
-    #include "../../../../Common/include/FlashRuntimeExtensions.h"
-    #define EXPORT __declspec(dllexport)
-    #define LOG_FILE "c:/hidapi.log"
 #endif
+#include "../../../../Common/include/FlashRuntimeExtensions.h"
 
-#define LOG_ENABLED false
+void print(const char * logfile, const char * filename, long line, const char * funcname, const char * msg, ...);
+void printResult(const char * logfile, const char * filename, long line, const char * funcname, const char * method, FREResult result);
+
+#define LOG_FILE "c:/hidapi.log"
+#define LOG_ENABLED
+
+#ifdef LOG_ENABLED
+    #define DEBUG_PRINT(filename, line, funcname, pattern, ...)         print(LOG_FILE, filename, line, funcname, pattern, ##__VA_ARGS__)
+    #define DEBUG_RESULT(filename, line, funcname, method, result)      printResult(LOG_FILE, filename, line, funcname, method, result)
+#else
+    #define DEBUG_PRINT(filename, line, funcname, pattern, ...)
+    #define DEBUG_RESULT(filename, line, funcname, method, result)
+#endif
 
 //export c-functions
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define EXPORT __declspec(dllexport)
 EXPORT void initializer(void** extData, FREContextInitializer* ctxInitializer, FREContextFinalizer* ctxFinalizer);
 EXPORT void finalizer(void* extData);
 
